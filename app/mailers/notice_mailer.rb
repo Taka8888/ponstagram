7edc97654e13795d8b663513b09582203a2149df
木下貴司 <takataka@TAKAnoMacBook-Pro.local>
2017-05-17 20:47:37 +0900

    notice mailer

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -5,16 +5,16 @@ class NoticeMailer < ApplicationMailer
   #
   #   en.notice_mailer.sendmail_picture.subject
   #     def sendmail_picture(picture)
-  　　def sendmail_picture(picture)
-        @picture = picture
-　　　　　　  mail to: "puravida590@gmail.com",
-               subject: '【Ponstagram】ポストが投稿されました'
-    end
-
-    def sendmail_contact(contact)
-        @contact = contact
-        mail to: @contact.email,
-        subject:'【Ponstagram】お問い合わせありがありました。'
-    end
+#   　　def sendmail_picture(picture)
+#         @picture = picture
+# 　　　　　　  mail to: "puravida590@gmail.com",
+#                subject: '【Ponstagram】ポストが投稿されました'
+#     end
+#
+#     def sendmail_contact(contact)
+#         @contact = contact
+#         mail to: @contact.email,
+#         subject:'【Ponstagram】お問い合わせありがありました。'
+#     end
 
   end
