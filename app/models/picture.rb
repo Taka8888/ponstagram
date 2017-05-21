42829295b3f372a6587b7b9ed2f3c4fb58025930
木下貴司 <[puravida590@gmail.com]>
2017-05-21 23:13:32 +0900

    validate picture title,content length

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,8 @@
 class Picture < ActiveRecord::Base
   # validates :avatar, presence: true
   validates :title, :avatar, :content, presence:true
+  validates :title, length: { maximum: 100 }
+  validates :content, length: { maximum: 500 }
   mount_uploader :avatar, AvatarUploader
    belongs_to :user
    def own?(user)
