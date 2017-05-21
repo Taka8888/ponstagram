48653ce70e33ce928dcd6f4ae41781b98aab7634
木下貴司 <takataka@TAKAnoMacBook-Pro.local>
2017-05-21 17:03:21 +0900

    change picture index

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
   # validates :avatar, presence: true
-    validates :title, :content, :avatar, presence:true
+  validates :title, :avatar, :content, presence:true
   mount_uploader :avatar, AvatarUploader
    belongs_to :user
    def own?(user)
