47674860f317e0ef6aaf8d08a80608c594b44787
木下貴司 <puravida590@gmail.com>
2017-12-06 18:02:00 +0900

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+100.times do |m|
+  password = "password"
+  avatar=Faker::Avatar.imageemail = Faker::Internet.email
+  user = User.create!(
+
+             name: name,
+              email: email,
+              password: password,
+              password_confirmation: password,
+              uid: m,
+              provider: m,
+              image_url: avatar
+
+              )
+
+              picture  = Picture.create!(
+
+              title: "いいいいいい",
+              user_id: user.id,
+              content: "ううううう",
+              avatar:  avatar
+              ）
